6089756fcbca2dd30585763eb182f220794456a6
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 14:58:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.go();
+        Bicycle bike = new Bicycle();
+        bike.stop();
+        System.out.println(car.speed);
+        System.out.println(bike.speed);
+        System.out.println(car.doors);
+        System.out.println(bike.pedals);
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    double speed;
+    void go(){
+        System.out.println("This vehicle moving");
+    }
+    void stop(){
+        System.out.println("This vehicle stop");
+    }
+}
